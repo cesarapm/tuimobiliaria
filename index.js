a7114c5534010e34fcc6aa71097f3f1e0a815df7
cesarapm <garyfeomm@gmail.com>
2023-06-13 15:49:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "*", // Reemplazar con dominio
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/hola", require("./routes/props"));
+
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
---
+++ routes/props.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const router = new Router();
+
+const fetch = require("node-fetch");
+
+router.get("/:id", async (req, res) => {
+  // const id = req.params.id;
+
+  // const url =
+  //   "https://api.easybroker.com/v1/properties?page=" +
+  //   `${id}` +
+  //   "&limit=20&search[statuses][]=published";
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     accept: "application/json",
+  //     "X-Authorization": "bsytg8rgtuuhm952r71yp0lxs9y46k",
+  //   },
+  // };
+
+  // fetch(url, options)
+  //   .then((rese) => rese.json())
+  //   .then((json) => res.json(json))
+  //   .catch((err) => console.error("error:" + err));
+  res.send("hello world");
+});
+
+module.exports = router;
