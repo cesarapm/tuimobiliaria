558e5669455d1061241f0910684cee3ed0b2b4b8
cesarapm <garyfeomm@gmail.com>
2023-06-13 15:54:55 -0500

    Add your commit

--- routes/props.js
+++ routes/props.js
@@ -3,26 +3,25 @@ const router = new Router();
 
 const fetch = require("node-fetch");
 
-router.get("/", async (req, res) => {
-  // const id = req.params.id;
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
 
-  // const url =
-  //   "https://api.easybroker.com/v1/properties?page=" +
-  //   `${id}` +
-  //   "&limit=20&search[statuses][]=published";
-  // const options = {
-  //   method: "GET",
-  //   headers: {
-  //     accept: "application/json",
-  //     "X-Authorization": "bsytg8rgtuuhm952r71yp0lxs9y46k",
-  //   },
-  // };
+  const url =
+    "https://api.easybroker.com/v1/properties?page=" +
+    `${id}` +
+    "&limit=20&search[statuses][]=published";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "X-Authorization": "bsytg8rgtuuhm952r71yp0lxs9y46k",
+    },
+  };
 
-  // fetch(url, options)
-  //   .then((rese) => rese.json())
-  //   .then((json) => res.json(json))
-  //   .catch((err) => console.error("error:" + err));
-  res.send("hello world");
+  fetch(url, options)
+    .then((rese) => rese.json())
+    .then((json) => res.json(json))
+    .catch((err) => console.error("error:" + err));
 });
 
 module.exports = router;
